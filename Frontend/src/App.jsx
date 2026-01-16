8c922eacdb7f1cfd28c8131dc244e938aef8a91b
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-16 18:20:32 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('https://ai-code-reviewer-mern-stack-2.onrender.com/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-reviewer-mern-stack-3.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
