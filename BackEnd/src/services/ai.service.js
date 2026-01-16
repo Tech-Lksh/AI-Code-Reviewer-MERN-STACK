bbc8d70bff5b9ba5a1c1e58ca510b1ad68df3423
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-16 18:42:35 +0530

    Update ai.service.js

--- BackEnd/src/services/ai.service.js
+++ BackEnd/src/services/ai.service.js
@@ -1,6 +1,8 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
+// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
+
+const genAI = new GoogleGenerativeAI("AIzaSyCEcgJC7b3W-gD1t-IRkBinpwxbjf7bakA");
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
